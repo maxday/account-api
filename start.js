31ecd9def0bf1cf4b342caf705e56e239cbf102c
Maxime David <maxime.david@bnc.ca>
2021-04-13 14:56:11 -0400

    add comment

--- start.js
+++ start.js
@@ -1,11 +1,12 @@
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+//Datadog config
 const ddOptions = {
     'response_code':true,
     'tags': ['app:account-api']
   }
-  
+
 const connectDatadog = require('connect-datadog')(ddOptions);
 app.use(connectDatadog);
 
